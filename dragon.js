3aaa820af4f34e4b3500537d2db28f881f4ba027
@kiyotocrypto <kiyotocrypto@gmail.com>
2017-10-24 15:17:45 +0800

    transaction

--- dragon.js
+++ dragon.js
@@ -1,9 +1,15 @@
+import {convert} from 'ethereumjs-units';
 import {
   sha3,
   privateToAddress,
   privateToPublic,
   publicToAddress,
   toChecksumAddress,
+  ecsign,
+  ecrecover,
+  toBuffer,
+  bufferToHex,
+  fromRpcSig,
 } from 'ethereumjs-util';
 import {createHash, randomBytes, createCipheriv, pbkdf2Sync, createDecipheriv} from 'crypto';
 import scrypt from 'scryptsy';
@@ -11,13 +17,18 @@ import uuid from 'uuid';
 import Buffer from 'buffer';
 import QRCode from 'qrcode';
 
-
 export {
+  convert,
   sha3,
   privateToAddress,
   privateToPublic,
   publicToAddress,
   toChecksumAddress,
+  ecsign,
+  ecrecover,
+  toBuffer,
+  bufferToHex,
+  fromRpcSig,
   createHash,
   randomBytes,
   createCipheriv,
